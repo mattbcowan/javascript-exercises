c81aaf393e8d2db1c873ae7802c481b5136a7f01
Matt Cowan <matt@mattbcowan.com>
2022-05-04 19:38:28 -0500

    02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  // If num = 0 return ''
+  if (num === 0) return "";
+  // If num < 0 return ERROR
+  if (num < 0) return "ERROR";
+  // Make a variable called result and set it to ''
+  let result = "";
+  // Make a for loop and loop for the duration of num
+  for (let i = 0; i < num; i++) {
+    // Inside the for loop add string to result
+    result = result + string;
+  }
+  // Once the loop is done return the result
+  return result;
 };
 
 // Do not edit below this line
