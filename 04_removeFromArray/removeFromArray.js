243a0cf11c99eeec283225f2434fd1baca64feb9
Matt Cowan <matt@mattbcowan.com>
2022-05-05 06:42:57 -0500

    04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+  // Initialize a new array named response
+  let response = [];
 
+  // Initialize a new array called options
+  // Add all additional arguments to options
+  let options = Object.values(arguments).slice(1);
+
+  // Loop over original array and check each
+  // value against the values in options.
+  for (let i = 0; i < arr.length; i++) {
+    // If the current value is not in options
+    // add it to response
+    if (!options.includes(arr[i])) {
+      response.push(arr[i]);
+    }
+  }
+
+  // return response
+  return response;
 };
 
 // Do not edit below this line
