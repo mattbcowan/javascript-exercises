d41bddf8641dd8512edd5d5e8ff753a52c40fb8c
Matt Cowan <matt@mattbcowan.com>
2022-05-05 06:15:23 -0500

    03_reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  // Split str into individual letters
+  str = str.split("");
 
+  // Initialize a variable called newStr
+  let newStr = [];
+
+  // For each letter in str starting from
+  // the end and moving to the beginning
+  // add the individual letters to newStr
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr.push(str[i]);
+  }
+
+  // Join the letters of newStr together
+  newStr = newStr.join("");
+  // Return newStr
+  return newStr;
 };
 
 // Do not edit below this line
